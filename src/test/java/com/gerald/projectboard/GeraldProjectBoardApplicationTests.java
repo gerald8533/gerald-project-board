e51f9a6cbda93028216ba814076ff8164a09ea62
gerald <gerald8533@gmail.com>
2025-10-16 14:05:24 +0900

    gitflow-feature-stash: #7-boot
    #7-스프링 부트 프로젝트 시작
    
    스프링 부트 프로젝트 초기화
    *스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ src/test/java/com/gerald/projectboard/GeraldProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerald.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeraldProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
